cd265bf9e33082741007571affcb7895e96bfb7f
Stefan Elmlinger <stefan.elmlinger@outlook.com>
2019-11-20 08:36:26 +0100

    add database connection

--- src/com/wwi118/HelloWorldServlet.java
+++ src/com/wwi118/HelloWorldServlet.java
@@ -1,6 +1,10 @@
 package com.wwi118;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,8 +17,25 @@ public class HelloWorldServlet extends HttpServlet{
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String message = req.getParameter("message");
+		System.out.println(message);
+		writeToDatabase(message);
 		
-		System.out.println(req.getParameter("message"));
+	}
+	
+	
+	private void writeToDatabase(String message) {
+		try {
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/messages?useSSL=false", "root", "start123");
+			Statement statement = conn.createStatement();
+			
+			String insert = "insert into messages(message) values ('" + message + "')";
+			
+			statement.executeUpdate(insert);
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
